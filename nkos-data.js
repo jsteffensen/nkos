04f833dc2ccac098d53923271408a953faaa20bb
jsteffensen <jsteffensen@users.noreply.github.com>
2021-11-09 16:55:15 +0100

    Update nkos-data.js

--- nkos-data.js
+++ nkos-data.js
@@ -1,5 +1,11 @@
 var userId, selectedNode, graph;
 
+function testClick() {
+  insertTestData2().then(function() {
+    updateForceGraph();
+  });
+}
+
 function clearData() {
   clearSelected();
   clearGraph();
@@ -24,8 +30,24 @@ function insertTestData() {
     selectedNode = {'title':'my node1', 'id':'1', 'class':'someType'};
     graph.nodes.push(selectedNode);
     graph.nodes.push({'title':'my node2', 'id':'2', 'class':'someOtherType'});
+    graph.nodes.push({'title':'my node3', 'id':'3', 'class':'someThirdType'});
 
-    graph.links.push({'source':'1', 'target':'2', 'predicate':'hasA'});
+    graph.links.push({'source':'1', 'target':'2', 'title':'hasA'});
+    graph.links.push({'source':'3', 'target':'1', 'title':'uses'});
+
+    resolve();
+  });
+  
+}
+
+function insertTestData2() {
+
+  return new Promise(function(resolve, reject) {
+
+    selectedNode = {'title':'my node2', 'id':'2', 'class':'someOtherType'};
+
+    graph.nodes.push({'title':'my node4', 'id':'4', 'class':'fourthType'});
+    graph.links.push({'source':'4', 'target':'2', 'title':'usedToBe'});
 
     resolve();
   });
